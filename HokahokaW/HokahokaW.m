33bde927f09f2a797bf23aacd3d5cf70e68b8c9a
Kenta Takagaki <kentaroh.takagaki@gmail.com>
2014-10-09 22:37:09 +0200

    Thresholding changes for Maite Freeze

--- HokahokaW/HokahokaW.m
+++ HokahokaW/HokahokaW.m
@@ -101,17 +101,17 @@ Options[HHThreshold]={HHThresholdLevel->Automatic, HHThresholdDirection->Automat
 
 
 (* ::Subsubsection:: *)
-(*HHDetectTrain*)
+(*HHPulseTrainDetect*)
 
 
-HHDetectTrain::usage="Detect a train.";
+HHPulseTrainDetect::usage="Detect a train.";
 
 
-HHTrainPulseLengthMinimum::usage="Number of different pulse lengths to accept.";
-HHTrainBlackout::usage="Number of samples from beginning (or {beginning, end}) to reject for thresholding.";
+HHPulseTrainLengthMinimum::usage="Number of different pulse lengths to accept.";
+HHPulseTrainBlackout::usage="Number of samples from beginning (or {beginning, end}) to reject for thresholding.";
 
 
-Options[HHDetectTrain]={HHTrainPulseLengthMinimum->0, HHTrainBlackout->None};
+Options[HHPulseTrainDetect]={HHPulseTrainLengthMinimum->0, HHPulseTrainBlackout->None};
 
 
 (* ::Subsection:: *)
@@ -372,11 +372,14 @@ HHThreshold[data_List, opts:OptionsPattern[]] :=  HHThreshold[data, Automatic, o
 HHThreshold[data_List, Automatic, opts:OptionsPattern[]] := HHThresholdImplSimpleAutoMedian[data];
 
 
+HHThreshold[data_List, thresholdValue_, opts:OptionsPattern[]] := HHThresholdImplSimple[data, thresholdValue];
+
+
 HHThreshold[args___]:=Message[HHThreshold::invalidArgs,{args}];
 
 
 HHThresholdImplSimpleAutoMedian[data_List]:=
-Module[{tempData, tempThresh, tempRes},
+Module[{tempData},
 	HHThresholdImplSimple[
 		tempData=data-Median[data],
 		HHStandardDeviationMedianEstimate[tempData]*4
@@ -394,29 +397,29 @@ Module[{tempRes},
 
 
 (* ::Subsubsection:: *)
-(*HHDetectTrain*)
+(*HHPulseTrainDetect*)
 
 
-HHDetectTrain[data_List, opts:OptionsPattern[]] := HHDetectTrain[data, Automatic, opts]
+HHPulseTrainDetect[data_List, opts:OptionsPattern[]] := HHPulseTrainDetect[data, Automatic, opts]
 
 
-HHDetectTrain[data_List, Automatic, opts:OptionsPattern[]] := 
+HHPulseTrainDetect[data_List, thresholdValue_, opts:OptionsPattern[]] := 
 Module[{threshed, tempRes,
 		optBlackout,optPulseMin},
 
-	threshed=HHThreshold[data, Automatic];
-	If[ Length[threshed]==0, Message[HHDetectTrain::noThresholdCrosses]];
-	If[ !EvenQ[Length[threshed]], Message[HHDetectTrain::oddThresholdCounts]];
+	threshed=HHThreshold[data, thresholdValue];
+	If[ Length[threshed]==0, Message[HHPulseTrainDetect::noThresholdCrosses]];
+	If[ !EvenQ[Length[threshed]], Message[HHPulseTrainDetect::oddThresholdCounts]];
 	tempRes= (#-{0,1})& /@ Partition[threshed,2];
 	
-	optBlackout = OptionValue[HHTrainBlackout];
+	optBlackout = OptionValue[HHPulseTrainBlackout];
 	If[ Head[optBlackout] === List && Length[optBlackout]==2,
 		tempRes = Select[ tempRes, (#[[2]]<optBlackout[[1]] && #[[1]]>optBlackout[[2]])&],
 		If[ Head[optBlackout]===Integer || Head[optBlackout] ===Real,
 			tempRes = Select[ tempRes, (#[[2]]>optBlackout)&]
 	]];
 	
-	optPulseMin = OptionValue[HHTrainPulseLengthMinimum];
+	optPulseMin = OptionValue[HHPulseTrainLengthMinimum];
 	If[ (Head[optPulseMin] === Integer || Head[optPulseMin] === Real),
 		If[ optPulseMin >= 1, tempRes = Select[ tempRes, (#[[2]]-#[[1]] >= optPulseMin)& ] ]
 	];
@@ -426,11 +429,11 @@ Module[{threshed, tempRes,
 ];
 
 
-HHDetectTrain::noThresholdCrosses="No threshold crosses detected";
-HHDetectTrain::oddThresholdCounts="Some error, odd number of thresholds should not occur with HHThreshold[]";
+HHPulseTrainDetect::noThresholdCrosses="No threshold crosses detected";
+HHPulseTrainDetect::oddThresholdCounts="Some error, odd number of thresholds should not occur with HHThreshold[]";
 
 
-HHDetectTrain[args___]:=Message[HHDetectTrain::invalidArgs,{args}];
+HHPulseTrainDetect[args___]:=Message[HHPulseTrainDetect::invalidArgs,{args}];
 
 
 (* ::Subsection:: *)
